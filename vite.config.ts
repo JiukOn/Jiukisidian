b77e108d1d38b2c9f968e7a934a9a52fa100f2e5
JiukOn <78093035+JiukOn@users.noreply.github.com>
2026-02-08 06:46:52 +0000

    ed

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,24 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/Jiukisidian/',
+  build: {
+    chunkSizeWarningLimit: 2000,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            if (id.includes('tldraw')) {
+              return 'tldraw'
+            }
+            if (id.includes('@tiptap')) {
+              return 'tiptap'
+            }
+            if (id.includes('react') || id.includes('lucide')) {
+              return 'vendor'
+            }
+          }
+        },
+      },
+    },
+  },
 })
